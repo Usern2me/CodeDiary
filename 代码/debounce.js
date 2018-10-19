cc64f994286ba1cf59931850f0db27ad93b80007
cocoroise <593996605@qq.com>
2018-10-19 10:28:58 +0800

    init

--- 代码/debounce.js
+++ 代码/debounce.js
@@ -1,12 +1,13 @@
 function debounce(func, wait, immediate) {
     let timeout, result
     let debounced = function () {
-        let context = this
+        let context = this // 保存上下文
         let args = arguments
 
         if (timeout) clearTimeout(timeout)
         if (immediate) {
             let callNow = !timeout
+            // 立即执行时取消定时器
             timeout = setTimeout(function () {
                 timeout = null
             }, wait)
